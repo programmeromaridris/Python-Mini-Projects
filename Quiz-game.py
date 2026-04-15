30da95d39711a8b4e5755a453e95234cdaff6027
Omar Idris <omeratahir10@gmail.com>
2026-04-15 23:14:17 +0300

    Doing a bunch of mini projects for experience

---
+++ Quiz-game.py
@@ -0,0 +1,51 @@
+import time
+
+playing = input("Are you playing? (yes/no) ")
+
+if playing != "yes":
+    time.sleep(0.7)
+    print("Please come back when you are ready to play.")
+    quit()
+
+time.sleep(0.7)
+print("Okay, let's play! :)")
+
+score = 0  
+answer = input("Who is the best football player in the world?")
+
+if answer != "ronaldo" and answer != "Ronaldo":
+    time.sleep(0.7)
+    print("Wrong! The correct answer is Ronaldo.")
+    quit()
+    
+score = 10
+time.sleep(0.7)
+print("Your score is: " + str(score))
+
+answer = input("What is the largest country in the world?")
+if answer != "russia" and answer != "Russia":
+    time.sleep(0.7)
+    print("Wrong! The correct answer is Russia.")
+    quit() 
+score = 20
+time.sleep(0.7)
+print("Your score is: " + str(score) + ". Next round is double points!")
+
+answer = input("Arachnophobia is the fear of what?")
+if answer != "spiders" and answer != "Spiders":
+    time.sleep(0.7)
+    print("Wrong! The correct answer is Spiders.")
+    quit()
+score = 40
+print("Your score is: " + str(score) + ". Last round wins all!")
+
+answer = input("What is the capital of Canda?")
+if answer != "ottawa" and answer != "Ottawa":
+    time.sleep(0.7)
+    print("Wrong! The correct answer is Ottawa.")
+    quit()
+score = 100
+time.sleep(0.7)
+print("Congratulations! Your final score is: " + str(score) + ". You are a quiz master!")     
+
+   
