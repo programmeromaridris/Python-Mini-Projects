62bc42148256043ed769db1ef3c383e087cd8d28
Omar Idris <omeratahir10@gmail.com>
2026-04-16 15:26:40 +0300

    made it better

--- number-guesser.py
+++ number-guesser.py
@@ -8,30 +8,34 @@ if top_of_range.isdigit():
     if top_of_range <= 0:
         print("Please type a number larger than 0 next time.")
         quit()
-else:
+else:    
     print("Please type a number next time.")
     quit()
         
         
 
-random_number = random.randint(0, top_of_range)
-guesses = 0
+random_number = random.randint(1, top_of_range)
 
 while True:
     user_guess = input("Take a guess:")
     if user_guess.isdigit():
       user_guess = int(user_guess)
+    elif user_guess == "q":
+     print("You quit the game! The correct number was:" + str(random_number))
+     quit() 
+      
     else:
         print("Please type a number next time.")
         continue
     
+    
     if user_guess == random_number:
         print("You got it correct!")
+        break
     else:
-        print("Sorry, you got it wrong!")
-        
-        
+        print("Sorry, you got it wrong! Keep going!")
+        continue
     
-    break
+
     
 
